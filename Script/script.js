ee311e91d17ea4cc7d3fe54945a3bb1c0b35c019
cpetersend89 <cpetersend@gmail.com>
2016-01-04 11:11:43 -0600

    JS Variable Assignment

--- Script/script.js
+++ Script/script.js
@@ -9,9 +9,23 @@ alert("Orlando, Florida");
 //JavaScript Variables Assignment
 
 var employeePay;
-employeePay = 22.50;
-
 var employeeHours;
+var weeklyPay;
+employeePay = 22.50;
 employeeHours = 37;
+weeklyPay = employeePay * employeeHours;
 
-console.log("$" + employeePay * employeeHours);
+console.log("Employee Pay" + " " + "$" + employeePay);
+console.log("Employee Hours" + " " + employeeHours);
+console.log("Weekly Pay" + " " + "$" + weeklyPay);
+
+var employeePay2;
+employeePay2 = prompt("Employee Pay?");
+
+console.log("Employee Pay" + " " + "$" + employeePay2);
+
+var employeeHours2;
+employeeHours2 = prompt("Total Hours");
+
+console.log("Employee Hours" + " " + employeeHours2);
+console.log("Weekly Pay" + " " + "$" + employeePay2 * employeeHours2);
