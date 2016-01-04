bbb922b13ca53c930f8fcf45ba3c4869ec6e0c4c
cpetersend89 <cpetersend@gmail.com>
2016-01-04 10:30:19 -0600

    JavaScript Lecture Assignment

---
+++ Script/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+//Welcome to JavaScript Assignment
+
+document.write("Christian Petersen");
+console.log("I am 26 years old");
+alert("Orlando, Florida");
+
+//JavaScript Variables Assignment
+
+var employeePay;
+employeePay = 22.50;
+
+var employeeHours;
+employeeHours = 37;
+
+console.log(employeePay * employeeHours);
